7b48c872f3d3dcb777a26ed37b3e656f60465767
sathvik031 <148201363+sathvik031@users.noreply.github.com>
2025-08-08 11:29:34 +0530

    Add files via upload

---
+++ Assignments/Assignment9.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+    internal class Assignment9
+    {
+
+        public static string ReverseByWords(string sentence)
+        {
+            string[] words = sentence.Split(',');
+            Array.Reverse(words);
+            string revString = string.Join(" ", words);
+            return revString;
+        }
+
+        static void Main(string[] args)
+        {
+            string sentence = ConsoleUtil.GetInputString("Enter the string to be reversed");
+            string reversedString = ReverseByWords(sentence);
+            Console.WriteLine(reversedString);
+        }
+    }
+}
